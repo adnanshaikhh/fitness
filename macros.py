95e7d1b4ccb891314248f2a614f481c96ed5d375
Adnan Shaikh <adnan.shaikh1806@gmail.com>
2018-02-27 23:15:40 +0530

    Added colors and eggs

--- macros.py
+++ macros.py
@@ -41,8 +41,12 @@
 
 # Proteins
 
+# BMR = 1500
+# 33% reduction = 1000
 import sys
 from tabulate import tabulate
+from colorama import init
+init()
 
 calorie_lim = 1000
 
@@ -56,15 +60,17 @@ macro_dict = {
     # Cals, Carbs, Protein, Fat
     # Chicken contains no carbs at all
     "chicken":[187, 0, 33, 4],
-    "eggs":[143, 0.7, 13, 10],
+    "eggs raw":[143, 0.7, 13, 10],
+    "eggs boiled":[155, 1.1, 13, 11],
     "paneer":[98, 3.4, 11, 4.3],
     # Neither does butter
     "butter":[717, 0.1, 0.9, 81],
     # Assuming it's sunflower
     # NOTE: 2tbsp = 1/8 cup = 29.57g 
+    # NOTE: Ladle = 12g, Normally put about 8g avg 
     "oil":[884, 0, 0, 100],
     # Assuming it's Chitale
-    "milk":[70, 5.35, 3.5, 3.9],
+    "milk":[69, 5.1, 3.6, 3.8],
 
     # Starchy
     "roti":[220, 44, 8, 2],
@@ -90,6 +96,7 @@ macro_dict = {
     "apple":[52, 14, 0.3, 0.2],
     "sweet lime":[43, 10, 1, 0],
     "kiwi":[61, 15, 1.1, 0.5],
+    "guava":[68, 14, 2.6, 1],
     
     # 1 scoop
     "whey":[120, 3, 24, 1]
@@ -124,7 +131,7 @@ with open(sys.argv[1]) as f:
             TOTAL_F += CURR_F
         else:
             print(food_item + "doesn't exist in the dict!")
-    table.append( ["TOTAL", "---", round(TOTAL_CALS, 2), \
+    table.append( ['\033[1m\033[36m' + "TOTAL", "---", round(TOTAL_CALS, 2), \
                                    round(TOTAL_C, 2),    \
                                    round(TOTAL_P, 2),    \
                                    round(TOTAL_F, 2)])
@@ -136,7 +143,7 @@ with open(sys.argv[1]) as f:
                                    "Protein",    \
                                    "Fats"]))
 
-    print("Cals left: " + str(round ((calorie_lim - TOTAL_CALS), 2)) + \
-          " Carbs left: " + str (round ((carb_lim - TOTAL_C), 2)) +    \
-          " Protein left: " + str (round ((pro_lim - TOTAL_P),2)) +    \
-          " Fats left: " + str (round((fat_lim - TOTAL_F),2)) )
+    print('\033[31m' + "Cals left: " + str(round ((calorie_lim - TOTAL_CALS), 2)) + \
+          '\033[32m' + " Carbs left: " + str (round ((carb_lim - TOTAL_C), 2)) +    \
+          '\033[33m' + " Protein left: " + str (round ((pro_lim - TOTAL_P),2)) +    \
+          '\033[35m' + " Fats left: " + str (round((fat_lim - TOTAL_F),2)) )
